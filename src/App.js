8724d72b2a590365f46fe1ef66e7aac47d7ed7d0
Michael Adams <mtadams007@gmail.com>
2020-04-03 17:58:49 -0400

    Up to module 6.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStore } from "redux";
+import logo from "./logo.svg";
+import "./App.css";
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counter);
+
+const handleClick = () => {
+  store.dispatch({ type: "INCREMENT" });
+};
 
 function App() {
+  const [x, setX] = React.useState(false);
+  store.subscribe(() => {
+    setX(!x);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <p>{store.getState()}</p>
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => {
+            handleClick();
+          }}
         >
-          Learn React
-        </a>
+          Plus 1
+        </button>
       </header>
     </div>
   );
